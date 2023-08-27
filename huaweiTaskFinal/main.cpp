dee52b10e7f0ffcfb7e27ff81a9ddeb72dd5c336
mykytkad <mykytkad@users.noreply.github.com>
2023-08-27 23:35:12 +0300

    add another explanations

--- huaweiTaskFinal/main.cpp
+++ huaweiTaskFinal/main.cpp
@@ -29,8 +29,9 @@ double loss(std::vector<double>& losses, std::discrete_distribution<>& d, std::m
 
 
 typedef boost::property<boost::edge_weight_t, double> EdgeWeightProperty;
-typedef boost::property<boost::vertex_distance_t, double> Distance;
-typedef boost::adjacency_list< boost::vecS, boost::vecS, boost::undirectedS, Distance, EdgeWeightProperty> Graph;
+//typedef boost::property<boost::vertex_distance_t, double> Distance;
+//typedef boost::adjacency_list< boost::vecS, boost::vecS, boost::undirectedS, Distance, EdgeWeightProperty> Graph;
+typedef boost::adjacency_list< boost::vecS, boost::vecS, boost::undirectedS, boost::no_property, EdgeWeightProperty> Graph;
 
 typedef boost::graph_traits<Graph>::vertex_iterator vertex_iter;
 typedef boost::graph_traits<Graph>::vertex_descriptor vertex_descriptor;
@@ -72,26 +73,40 @@ public:
 			std::cout << "root vertex out of range" << std::endl;
 			return 0;
 		}
+		
+		// Dijkstra for the graph with delay as an edge weight 
+
 		boost::property_map< Graph, boost::edge_weight_t >::type weightmap = get(boost::edge_weight, *G_delay);
-		std::vector< vertex_descriptor > parents_delay(num_vertices(*G_delay));
-		std::vector< double > distance_delay(num_vertices(*G_delay));
-		vertex_descriptor s = vertex(root, *G_delay);
+		
+		std::vector< vertex_descriptor > parents_delay(num_vertices(*G_delay));  // vector of parent vertices in delay graph SPT
+		std::vector< double > distance_delay(num_vertices(*G_delay));            // vector of distances to the vertices of the delay graph
+		
+		vertex_descriptor s = vertex(root, *G_delay);  // start vertex
+		
+
 		auto predecessor = boost::predecessor_map(boost::make_iterator_property_map(parents_delay.begin(), get(boost::vertex_index, *G_delay)))
 			.distance_map(boost::make_iterator_property_map(
 			distance_delay.begin(), get(boost::vertex_index, *G_delay)));
 
 		boost::dijkstra_shortest_paths(*G_delay, s, predecessor);
 
+
+		// Dijkstra for the graph with loss as an edge weight
 		weightmap = get(boost::edge_weight, *G_loss);
-		std::vector< vertex_descriptor > parents_loss(num_vertices(*G_loss));
-		std::vector< double > distance_loss(num_vertices(*G_loss));
+		
+		std::vector< vertex_descriptor > parents_loss(num_vertices(*G_loss));    // vector of parent vertices in loss graph SPT
+		std::vector< double > distance_loss(num_vertices(*G_loss));              // vector of distances to the vertices of the loss graph
+		
 		s = vertex(root, *G_loss);
+
 		predecessor = boost::predecessor_map(boost::make_iterator_property_map(parents_loss.begin(), get(boost::vertex_index, *G_loss))).distance_map(boost::make_iterator_property_map(
 			distance_loss.begin(), get(boost::vertex_index, *G_loss)));
 
 		boost::dijkstra_shortest_paths(*G_loss, s, predecessor);
 
-		Graph delay_SPT(boost::num_vertices(*G_loss)); //with losses as edge weight
+
+		// Construction of SPT for delay graph with losses as edge weight
+		Graph delay_SPT(boost::num_vertices(*G_loss));   //SPT-graph with losses as edge weight
 		for (int i = 0; i < parents_delay.size(); i++)
 		{
 			if (i == parents_delay[i]) { //parent vertex of the root is root 
@@ -101,8 +116,10 @@ public:
 		}
 
 		weightmap = get(boost::edge_weight, delay_SPT);
+
 		std::vector< vertex_descriptor > parents_spt(num_vertices(delay_SPT));
 		std::vector< double > distance_spt(num_vertices(delay_SPT));
+		
 		s = vertex(root, delay_SPT);
 		predecessor = boost::predecessor_map(boost::make_iterator_property_map(parents_loss.begin(), get(boost::vertex_index, delay_SPT))).distance_map(boost::make_iterator_property_map(
 			distance_loss.begin(), get(boost::vertex_index, delay_SPT)));
@@ -116,10 +133,12 @@ public:
 				optimal_routes_num++;
 			}
 		}
-		//return double(optimal_routes_num) / boost::num_vertices(*G_loss);
-		return optimal_routes_num; 
+		//return double(optimal_routes_num) / boost::num_vertices(*G_loss); 
+
+		return optimal_routes_num;    // for optimisation I will divide it once in the end 
 	}
 };
+
 
 void two_grids_constructor(Graph& G_delay, Graph& G_loss, int N, std::vector<double>& losses, std::normal_distribution<double>& norm_d, std::discrete_distribution<>& discr_d)
 {
@@ -159,16 +178,13 @@ int main()
 	* 
 	* so variance should be also much lower.
 	*/
-
-
-	//const double EPSILON = 0.05 / (N * N);
 	
 	std::vector<double> losses = { 0, 0.01, 0.02, 0.03, 0.04, 0.05 };
 	std::normal_distribution<double> norm_d {7.5, 1.25};
 	std::discrete_distribution<> discr_d({ 0.5, 0.1, 0.1, 0.1, 0.1, 0.1 });
 
 
-	std::vector<double> optimal_routes_average(N);
+	std::vector<double> optimal_routes_average(N); // vector with number of optimal routes for first N vertices (boundary vertices)
 
 	time_t start, end;
 	time(&start);
