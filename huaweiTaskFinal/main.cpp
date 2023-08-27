68a46d0bbd4c0d3e627204c13fc88540219c1f2d
mykytkad <mykytkad@users.noreply.github.com>
2023-08-27 03:39:15 +0300

    Final (?) version with explanation in comments

--- huaweiTaskFinal/main.cpp
+++ huaweiTaskFinal/main.cpp
@@ -13,7 +13,7 @@
 
 double delay(std::normal_distribution<double>& d, std::mt19937& gen) {
 	double temp = d(gen);
-	return temp < 0 ? 0 : temp;
+	return temp < 0 ? 0 : temp;          // No sense in negative delay edges
 }
 
 double loss(std::vector<double>& losses, std::discrete_distribution<>& d, std::mt19937& gen)
@@ -26,12 +26,16 @@ double loss(std::vector<double>& losses, std::discrete_distribution<>& d, std::m
 	return losses[temp];
 }
 
+
+
 typedef boost::property<boost::edge_weight_t, double> EdgeWeightProperty;
 typedef boost::property<boost::vertex_distance_t, double> Distance;
 typedef boost::adjacency_list< boost::vecS, boost::vecS, boost::undirectedS, Distance, EdgeWeightProperty> Graph;
 
 typedef boost::graph_traits<Graph>::vertex_iterator vertex_iter;
 typedef boost::graph_traits<Graph>::vertex_descriptor vertex_descriptor;
+
+
 
 class Solution
 {
@@ -44,11 +48,29 @@ public:
 		G_delay = &d;
 		G_loss = &l;
 	}
+	~Solution() {
+
+	}
+
+	/*
+	* Firstly, I will calculate the shortest paths with delay and with loss as a metric,
+	* 
+	* then I will construct SPT of delay metric and fill the edges weight with losses, and calculate loss distances
+	* 
+	* in the delay tree. 
+	* 
+	* To compare the cost of paths in losses to every vertex I will compare final loss value.
+	* 
+	* It is easier than path comparison and it prevents a situation, when another path, but with the same cost value 
+	* 
+	* is considered as wrong path.
+	* 
+	* To solve this task I use boost library.
+	*/
 	
-	
-	double optimal_routes(int root)
+	int optimal_routes(int root)
 	{
-		if (root > boost::num_vertices(*G_delay))
+		if (root >= boost::num_vertices(*G_delay))
 		{
 			std::cout << "root vertex out of range" << std::endl;
 			return 0;
@@ -75,7 +97,7 @@ public:
 		Graph delay_SPT(boost::num_vertices(*G_loss)); //with losses as edge weight
 		for (int i = 0; i < parents_delay.size(); i++)
 		{
-			if (i == parents_delay[i]) { //parent vertex of the root vertex of Dijkstra is root vertex
+			if (i == parents_delay[i]) { //parent vertex of the root is root 
 				continue;
 			}
 			add_edge(i, parents_delay[i], get(boost::edge_weight_t(), *G_loss, boost::edge(i, parents_delay[i], *G_loss).first), delay_SPT);
@@ -97,8 +119,8 @@ public:
 				optimal_routes_num++;
 			}
 		}
-		return double(optimal_routes_num) / boost::num_vertices(*G_loss);
-		//return optimal_routes_num;
+		//return double(optimal_routes_num) / boost::num_vertices(*G_loss);
+		return optimal_routes_num; 
 	}
 };
 
@@ -128,7 +150,20 @@ void two_grids_constructor(Graph& G_delay, Graph& G_loss, int N, std::vector<dou
 int main()
 {
 	const int N = 32;
-	int NUM_EXPERIMENTS = 20;
+	int NUM_EXPERIMENTS = 100;
+
+	/* To determine the number of experiments that we need I used the Chebyshev theorem.
+	* 
+	* It requires an estimate of the variance, which I roughly estimated as (10/1000)^2.
+	* 
+	* With 100 experiments, this should be 0.002 precision of E estimation with 0.75 probability.
+	* 
+	* But after launching this program many times and compare results, I think, the precision is much lower,
+	* 
+	* so variance should be also much lower.
+	*/
+
+
 	//const double EPSILON = 0.05 / (N * N);
 	
 	std::vector<double> losses = { 0, 0.01, 0.02, 0.03, 0.04, 0.05 };
@@ -140,6 +175,16 @@ int main()
 
 	time_t start, end;
 	time(&start);
+
+
+	/*
+	* Optimal routes for bound vertices of the same graph are not independent,
+	* 
+	* but it does not matter, because to i-vertex results I add only new i-vertex result computed
+	* 
+	* on the new graph.
+	*/
+
 
 	for(int i = 0; i < NUM_EXPERIMENTS; i++)
 	{
@@ -162,15 +207,14 @@ int main()
 	
 	for (int i = 0; i < N; i++)
 	{
-		std::cout << "average for vertex " << i << " : " << optimal_routes_average[i] / NUM_EXPERIMENTS << std::endl;
+		std::cout << "average for vertex " << i << " : " << optimal_routes_average[i] / (NUM_EXPERIMENTS * N * N) << std::endl;
 	}
 	
-	double time_taken = double(end - start);
+	/*double time_taken = double(end - start);
 	std::cout << "Time  : "
 		<< time_taken << " ";
 	std::cout << " sec " << std::endl;
-	
-	std::cout << "num exper = " << NUM_EXPERIMENTS << std::endl;
+	*/
 	
 	return 0;
 }
